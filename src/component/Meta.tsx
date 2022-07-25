16b373b71fbd7c08c046522003ea7f79c88e96dc
yoshiaki <yoshiosmile68@gmail.com>
2022-07-25 14:36:44 +0900

    改善

--- src/component/Meta.tsx
+++ src/component/Meta.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 type Props = {
   title: string;
-  description: string;
+  description?: string;
   ogImage?: string;
 };
 
@@ -11,9 +11,13 @@ export const Meta = (props: Props) => {
     <Head>
       <title>{props.title}</title>
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <meta property="description" content={props.description} />
+      {props.description && (
+        <meta property="description" content={props.description} />
+      )}
       <meta property="og:title" content={props.title} />
-      <meta property="og:description" content={props.description} />
+      {props.description && (
+        <meta property="og:description" content={props.description} />
+      )}
       {props.ogImage && <meta property="og:image" content={props.ogImage} />}
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
