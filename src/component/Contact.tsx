4c21f83d33e7433f2ea84a782fed8433de4a87e3
yumotoyoshiaki <yoshiosmile68@gmail.com>
2022-11-13 14:51:54 +0900

    fix

--- src/component/Contact.tsx
+++ src/component/Contact.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Contact = () => {
   return (
-    <div className="sahdow-xl mx-auto space-y-4 rounded-lg bg-gray-800  p-[50px] text-white md:w-[1000px] md:text-center">
+    <div className="mx-auto space-y-4 rounded-lg bg-gray-800 p-[50px] text-white shadow-xl md:w-[1000px] md:text-center">
       <p className="text-3xl font-bold">CONTACT</p>
       <p className="text-xl">
         開発依頼・お見積り。その他、お問い合わせは
