5ac435e76e9a3fe5be8aa407cb4ea0e57a7496d3
yumotoyoshiaki <yoshiosmile68@gmail.com>
2023-01-23 16:45:13 +0900

    🔥

--- pages/privacy.tsx
+++
@@ -1,7 +0,0 @@
-import { Privacy } from "src/pages/privacy";
-
-const PrivacyPage = () => {
-  return <Privacy />;
-};
-
-export default PrivacyPage;
--- pages/team.tsx
+++
@@ -1,7 +0,0 @@
-import { Team } from "src/pages/team";
-
-const TeamPage = () => {
-  return <Team />;
-};
-
-export default TeamPage;
